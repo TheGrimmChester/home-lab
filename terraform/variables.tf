0d3018c9d75d3219081eb902723fcbbee8f1a980
TheGrimmChester <1076333+TheGrimmChester@users.noreply.github.com>
2024-10-09 19:22:25 +0200

    Added proxmox terraform

---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "cluster_name" {
+  type    = string
+  default = "home-lab"
+}
+
+variable "default_gateway" {
+  type    = string
+  default = "192.168.1.254"
+}
+
+variable "talos_cp_01_ip_addr" {
+  type    = string
+  default = "192.168.1.29"
+}
+
+variable "talos_worker_01_ip_addr" {
+  type    = string
+  default = "192.168.1.30"
+}
